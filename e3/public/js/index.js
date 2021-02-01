43757bc077b6bcb637082e52abd9d9b644a8e0dd
Miguel Mercado <miguel.mercadosalgado@gmail.com>
2021-02-01 21:11:46 +0100

    created topten html

--- e3/public/js/index.js
+++ e3/public/js/index.js
@@ -26,11 +26,37 @@ const getRegionCountries = (region) => {
     .catch((err) => console.error(err.message));
 };
 
+const getTenCountries = () => {
+  fetch("https://restcountries.eu/rest/v2/all")
+    .then((response) => response.json())
+    .then((data) => {
+      let list = document.querySelector("#countries");
+      let areaArr = data
+        .map((country) => [country.name, country.area])
+        .sort(compareNumbers);
+      let countriesList = areaArr
+        .slice(0, 10)
+        .map(
+          (country) => `<li>${country[0]} - ${country[1]} m<sup>2</sup></li>`
+        )
+        .join("");
+      list.innerHTML = countriesList;
+    })
+    .finally(() => console.log("Fetch done"))
+    .catch((err) => console.error(err.message));
+};
+
+function compareNumbers(a, b) {
+  return b[1] - a[1];
+}
+
 let countries = document.querySelector("#countries");
 const region = countries.getAttribute("data-region");
 
 if (region === "world") {
   getAllCountries();
+} else if (region === "topten") {
+  getTenCountries();
 } else {
   getRegionCountries(region);
 }
