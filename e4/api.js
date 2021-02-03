9d2a02df8888642eb1d58e89ab9b282fac724eee
Miguel Mercado <miguel.mercadosalgado@gmail.com>
2021-02-03 20:02:33 +0100

    DB created, CORS Policy

--- e4/api.js
+++ e4/api.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const api = express();
 
+api.use((request, response, next) => {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+  api.options("*", (request, response) => {
+    response.header(
+      "Access-Control-Allow-Methods",
+      "GET, PATCH, PUT, POST, DELETE, OPTIONS"
+    );
+    response.send();
+  });
+});
+
 api.get("/api/films", (request, response) => {
   response.status(200).send({
     success: true,
